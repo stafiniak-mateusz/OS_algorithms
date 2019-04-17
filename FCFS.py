f8dd76792730985fb577997707c672f0ba992f2f
Mateusz Stafiniak <234937@student.pwr.edu.pl>
2019-04-17 10:11:54 +0200

    Less c-like

--- FCFS.py
+++ FCFS.py
@@ -1,23 +1,22 @@
 from queue import Queue
 from Process import Process
+
+
 class FCFS:
-    def __init__(self, process_list = []):
+    def __init__(self, process_list=[]):
         self.process_list = process_list
-        
+
     def main_loop(self):
         q = Queue()
         status = False
-        tmp = Process(0,0)
+        tmp = Process(0, 0)
         for i in range(Process.total_burst_time):
 
-            for j in range(len(self.process_list)):
-
-                if self.process_list[j].arrival_time == i:
-
-                    q.put(self.process_list[j])
+            for p in self.process_list:
+                if p.arrival_time == i:
+                    q.put(p)
                     print("\nProces dodany do kolejki")
-            
-            
+
             if not status and not q.empty():
                 tmp = q.get()
                 status = True
@@ -25,14 +24,12 @@ class FCFS:
 
             tmp.burst_time -= 1
 
-            print("P"+str(tmp.number)+": "+str(tmp.burst_time))
-
+            print(f"P{tmp.number+1}: {tmp.burst_time}")
             if tmp.burst_time == 0:
-                print("Czas oczekiwania: " + str(tmp.waiting_time))
+                print(f"Czas oczekiwania: {tmp.waiting_time}")
                 status = False
 
             for var in self.process_list:
                 var.waiting_time += 1
         Process.proces_number = -1
         Process.total_burst_time = 0
-
--- SJF.py
+++ SJF.py
@@ -1,30 +1,30 @@
 from heapq import heappush, heappop
 from Process import Process
+
+
 class SJF:
-    def __init__(self, process_list = []):
+    def __init__(self, process_list=[]):
         self.process_list = process_list
 
     def main_loop(self):
         heap = []
-        tmp = Process(0,0)
+        tmp = Process(0, 0)
         for i in range(Process.total_burst_time):
-            
-            for j in range(Process.proces_number):
-                if self.process_list[j].arrival_time == i:
-                    heappush(heap, self.process_list[j])
+
+            for p in self.process_list:
+                if p.arrival_time == i:
+                    heappush(heap, p)
                     print("\nProces dodany do kopca")
-                        
-            
-            tmp = heappop(heap)
-            
+
+            if len(heap) != 0:
+                tmp = heappop(heap)
+
             tmp.burst_time -= 1
-            print("P"+str(tmp.number+1)+": "+str(tmp.burst_time))
+            print(f"P{tmp.number+1}: {tmp.burst_time}")
             heappush(heap, tmp)
 
-           
-
             if tmp.burst_time == 0:
-                print("P"+str(tmp.number+1)+"Czas oczekiwania: " + str(tmp.waiting_time))
+                print(f"P{tmp.number+1} - Czas oczekiwania: {tmp.waiting_time}")
                 heappop(heap)
 
             for var in heap:
@@ -32,5 +32,3 @@ class SJF:
 
         Process.proces_number = -1
         Process.total_burst_time = 0
-            
-           
